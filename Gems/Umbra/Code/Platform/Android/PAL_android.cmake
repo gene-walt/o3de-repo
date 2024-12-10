c67321a0ca73b69c4e6bb800162fe2c8f0cb2ab5
Junhao Wang <wjunhao@amazon.com>
2024-12-10 11:35:15 -0800

    Update CMake to support macOS, iOS, Android
    cr: https://code.amazon.com/reviews/CR-165123071

--- Gems/Umbra/Code/Platform/Android/PAL_android.cmake
+++ Gems/Umbra/Code/Platform/Android/PAL_android.cmake
@@ -3,6 +3,21 @@
 # its licensors.
 #
 
-set(PAL_TRAIT_UMBRA_SUPPORTED FALSE)
+set(PAL_TRAIT_UMBRA_SUPPORTED TRUE)
 set(PAL_TRAIT_UMBRA_TEST_SUPPORTED FALSE)
 set(PAL_TRAIT_UMBRA_EDITOR_TEST_SUPPORTED FALSE)
+
+set(UMBRA_TARGET "3rdParty::umbra")
+
+set(UMBRA_LIBRARIES_DEBUG
+    )
+set(UMBRA_LIBRARIES_RELEASE
+    )
+
+# This is for arm64. You should change the folder name to 'android-armeabi-v7a' if you use x86.
+set(UMBRA_STATIC_LIBRARIES_DEBUG
+    ${UMBRA_SDK_PATH}/lib/android-arm64-v8a/libumbraruntime_d.a
+    ${UMBRA_SDK_PATH}/lib/android-arm64-v8a/libumbracommon_d.a)
+set(UMBRA_STATIC_LIBRARIES_RELEASE
+    ${UMBRA_SDK_PATH}/lib/android-arm64-v8a/libumbraruntime.a
+    ${UMBRA_SDK_PATH}/lib/android-arm64-v8a/libumbracommon.a)
--- Gems/Umbra/Code/Platform/iOS/PAL_ios.cmake
+++ Gems/Umbra/Code/Platform/iOS/PAL_ios.cmake
@@ -3,6 +3,19 @@
 # its licensors.
 #
 
-set(PAL_TRAIT_UMBRA_SUPPORTED FALSE)
+set(PAL_TRAIT_UMBRA_SUPPORTED TRUE)
 set(PAL_TRAIT_UMBRA_TEST_SUPPORTED FALSE)
 set(PAL_TRAIT_UMBRA_EDITOR_TEST_SUPPORTED FALSE)
+
+set(UMBRA_TARGET "3rdParty::umbra")
+
+set(UMBRA_LIBRARIES_DEBUG
+    )
+set(UMBRA_LIBRARIES_RELEASE
+    )
+set(UMBRA_STATIC_LIBRARIES_DEBUG
+    ${UMBRA_SDK_PATH}/lib/ios/Debug/libumbraruntime.a
+    ${UMBRA_SDK_PATH}/lib/ios/Debug/libumbracommon.a)
+set(UMBRA_STATIC_LIBRARIES_RELEASE
+    ${UMBRA_SDK_PATH}/lib/ios/Release/libumbraruntime.a
+    ${UMBRA_SDK_PATH}/lib/ios/Release/libumbracommon.a)
